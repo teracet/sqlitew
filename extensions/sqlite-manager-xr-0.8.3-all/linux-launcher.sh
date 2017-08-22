63ec23c9ff392b51ef04c143dd121be6af1e8e35
Jamie Syme <jamie.d.syme@gmail.com>
2017-08-22 13:46:33 -0700

    Updated the linux launcher so that it can be called from anywhere.

--- extensions/sqlite-manager-xr-0.8.3-all/linux-launcher.sh
+++ extensions/sqlite-manager-xr-0.8.3-all/linux-launcher.sh
@@ -1,3 +1,4 @@
-PROFILE_DIR=~/.sqlite-composer/profile
+APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROFILE_DIR=$APP_DIR/profiles/sqlite-composer
 mkdir -p $PROFILE_DIR
-./browser-bin --app apps/sqlite-manager/application.ini --no-remote --profile $PROFILE_DIR
+$APP_DIR/browser-bin --app $APP_DIR/apps/sqlite-manager/application.ini --no-remote --profile $PROFILE_DIR
