654518f1ef6401df03f3cc57af1f357c5ac201ba
Jamie Syme <jamie.d.syme@gmail.com>
2017-08-23 18:04:44 -0700

    Replaced placeholder icons.

--- configure-browser.sh
+++ configure-browser.sh
@@ -33,20 +33,28 @@ cp "$REPO_DIR/config/mozilla.cfg" "$INSTALL_DIR"
 # Copy the source.
 
 EXT_DIR="$REPO_DIR/extensions/sqlite-manager-xr-0.8.3-all"
-DEST_DIR="$APPS_DIR/sqlite-manager"
-rm -rf "$DEST_DIR" && mkdir -p "$DEST_DIR"
-cp -r "$EXT_DIR/source/"* "$DEST_DIR"
+APP_DIR="$APPS_DIR/sqlite-manager"
+rm -rf "$APP_DIR" && mkdir -p "$APP_DIR"
+cp -r "$EXT_DIR/source/"* "$APP_DIR"
 
-# Update the application.ini, branding, and icons.
+# Update the application.ini and other branding.
 
-cp "$EXT_DIR/application.ini" "$DEST_DIR"
-grep -rl "SQLite Manager" "$DEST_DIR" | xargs sed -i 's/SQLite Manager/SQLite Composer/g'
-cp -r "$INSTALL_DIR/browser/chrome/icons/"* "$DEST_DIR/icons"
+cp "$EXT_DIR/application.ini" "$APP_DIR"
+grep -rl "SQLite Manager" "$APP_DIR" | xargs sed -i 's/SQLite Manager/SQLite Composer/g'
+
+# Update the icons.
+
+rm -f "$APP_DIR/chrome/icons/default/"*
+cp "$EXT_DIR/icons/icon_16x16.png" "$APP_DIR/chrome/icons/default/default16.png"
+cp "$EXT_DIR/icons/icon_32x32.png" "$APP_DIR/chrome/icons/default/default32.png"
+cp "$EXT_DIR/icons/icon_128x128.png" "$APP_DIR/chrome/icons/default/default48.png" # TODO: Replace with real 48x48 icon
+mkdir -p "$APP_DIR/icons"
+cp "$EXT_DIR/icons/icon_128x128.png" "$APP_DIR/icons/mozicon128.png"
 
 # Since we are not "properly" installing the extension, there is an unexpected
 # exception that gets thrown; let's patch that.
 
-sed -i 's/^.*SmAppInfo.extVersion =.*$/SmAppInfo.extVersion = (addon || {}).version || "xxx";/' "$DEST_DIR/chrome/resource/appInfo.js"
+sed -i 's/^.*SmAppInfo.extVersion =.*$/SmAppInfo.extVersion = (addon || {}).version || "xxx";/' "$APP_DIR/chrome/resource/appInfo.js"
 
 # Copy the launcher files.
 
--- extensions/sqlite-manager-xr-0.8.3-all/linux-launcher.sh
+++ extensions/sqlite-manager-xr-0.8.3-all/linux-launcher.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+APP_DIR="$ROOT_DIR/apps/sqlite-manager"
 
 # Update the .desktop file icon
-ICON_PATH="$APP_DIR/browser/icons/mozicon128.png"
-sed -i -e "s|^Icon=$|Icon=$ICON_PATH|" "$APP_DIR/sqlite-composer.desktop"
+ICON_PATH="$APP_DIR/icons/mozicon128.png"
+sed -i -e "s|^Icon=$|Icon=$ICON_PATH|" "$ROOT_DIR/sqlite-composer.desktop"
 
 # Launch the app
-PROFILE_DIR="$APP_DIR/profiles/sqlite-composer"
+PROFILE_DIR="$ROOT_DIR/profiles/sqlite-composer"
 mkdir -p "$PROFILE_DIR"
-"$APP_DIR/browser-bin" --app "$APP_DIR/apps/sqlite-manager/application.ini" --no-remote --profile "$PROFILE_DIR"
+"$ROOT_DIR/browser-bin" --app "$APP_DIR/application.ini" --no-remote --profile "$PROFILE_DIR"
