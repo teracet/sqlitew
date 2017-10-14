1149126baca4912eed1b86ecc68fbc7f264bc4e9
Jamie Syme <jamie.d.syme@gmail.com>
2017-10-14 11:39:40 -0400

    Mac build and installer are working.

--- mac/compile-browser.sh
+++ mac/compile-browser.sh
@@ -5,8 +5,10 @@ REPO_DIR="$MAC_DIR/.."
 FIREFOX_VERSION=54.0.1
 BUILD_DIR="$REPO_DIR/build"      # Directory where we can work
 SOURCE_DIR="$BUILD_DIR/source"   # Destination for the firefox code
-INSTALL_DIR="$SOURCE_DIR/obj-sqlite-composer/dist/bin"
-APPS_DIR="$INSTALL_DIR/apps"
+BUNDLE_DIR="$SOURCE_DIR/obj-sqlite-composer/dist/SQLiteComposer.app"
+BUNDLE_BIN_DIR="$BUNDLE_DIR/Contents/MacOS"
+BUNDLE_RES_DIR="$BUNDLE_DIR/Contents/Resources"
+APPS_DIR="$BUNDLE_RES_DIR/apps"
 ICON_DIR="$REPO_DIR/extensions/sqlite-manager-xr-0.8.3-all/icons"
 
 
@@ -14,12 +16,12 @@ ICON_DIR="$REPO_DIR/extensions/sqlite-manager-xr-0.8.3-all/icons"
 
 DOWNLOAD_URL="https://archive.mozilla.org/pub/firefox/releases/$FIREFOX_VERSION/source/firefox-$FIREFOX_VERSION.source.tar.xz"
 FILE=firefox-source.tar.xz
-#rm -rf "$BUILD_DIR" && mkdir -p "$BUILD_DIR"
-#rm -rf "$SOURCE_DIR" && mkdir -p "$SOURCE_DIR"
-#cd "$BUILD_DIR"
-#curl -o "$FILE" "$DOWNLOAD_URL"
-#tar -xf "$FILE" --directory "$SOURCE_DIR" --strip-components=1
-#rm "$FILE"
+rm -rf "$BUILD_DIR" && mkdir -p "$BUILD_DIR"
+rm -rf "$SOURCE_DIR" && mkdir -p "$SOURCE_DIR"
+cd "$BUILD_DIR"
+curl -o "$FILE" "$DOWNLOAD_URL"
+tar -xf "$FILE" --directory "$SOURCE_DIR" --strip-components=1
+rm "$FILE"
 unset DOWNLOAD_URL
 unset FILE
 
@@ -29,12 +31,12 @@ unset FILE
 # Part of the bootstrap process requires Xcode. If Xcode was installed but not
 # used yet, we need to specify the app directory.
 echo 'Selecting Xcode app directory (requires sudo privileges).'
-#sudo xcode-select --switch '/Applications/Xcode.app'
+sudo xcode-select --switch '/Applications/Xcode.app'
 
 # Bootstrapping fails when installing rust on a Mac, so we will install it
 # manually ahead of time.
 echo 'Installing rust.'
-#brew install rust
+brew install rust
 
 # In order to build Firefox, we need to install all of the libraries/tools it
 # depends on.
@@ -46,7 +48,7 @@ echo 'Installing rust.'
 #   Response #3: '2' - Do not configure Mercurial
 
 cd "$SOURCE_DIR"
-#printf "2\n1\n2\n" | ./mach bootstrap
+printf "2\n1\n2\n" | ./mach bootstrap
 
 
 # PATCH: SQLITE FLAGS
@@ -69,12 +71,13 @@ cp -r "$SOURCE_DIR/browser/branding/unofficial" "$BRANDING_DIR"
 # All of the changes in this section are to update any references to "Firefox"
 # or "Mozilla" that will be spread when we compile.
 
-sed -i '.original' 's/"Nightly"/"SQLite Composer"/'       "$BRANDING_DIR/locales/en-US/brand.dtd"
-sed -i '.original' 's/=Nightly/=SQLite Composer/'         "$BRANDING_DIR/locales/en-US/brand.properties"
-sed -i '.original' 's/"Mozilla"/"Teracet"/'               "$BRANDING_DIR/locales/en-US/brand.dtd"
-sed -i '.original' 's/=Mozilla/=Teracet/'                 "$BRANDING_DIR/locales/en-US/brand.properties"
-sed -i '.original' 's/Nightly/"SQLite Composer"/'         "$BRANDING_DIR/configure.sh"
-echo 'MOZ_APP_NAME="sqlite-composer-bin"'              >> "$BRANDING_DIR/configure.sh"
+sed -i '' 's/"Nightly"/"SQLite Composer"/'         "$BRANDING_DIR/locales/en-US/brand.dtd"
+sed -i '' 's/=Nightly/=SQLite Composer/'           "$BRANDING_DIR/locales/en-US/brand.properties"
+sed -i '' 's/"Mozilla"/"Teracet"/'                 "$BRANDING_DIR/locales/en-US/brand.dtd"
+sed -i '' 's/=Mozilla/=Teracet/'                   "$BRANDING_DIR/locales/en-US/brand.properties"
+sed -i '' 's/Nightly/"SQLite Composer"/'           "$BRANDING_DIR/configure.sh"
+echo 'MOZ_APP_NAME="sqlite-composer-bin"'       >> "$BRANDING_DIR/configure.sh"
+#echo 'MOZ_MACBUNDLE_NAME="SQLite Composer.app"' >> "$BRANDING_DIR/configure.sh" # Doesn't work
 
 cp "$ICON_DIR/mac/firefox.icns" "$BRANDING_DIR/firefox.icns"
 
@@ -95,9 +98,11 @@ echo '0.0.1' > "$SOURCE_DIR/browser/config/version_display.txt"
 
 PACKAGE_MANIFEST="$SOURCE_DIR/browser/installer/package-manifest.in"
 echo '[sqlite-composer]'                          >> "$PACKAGE_MANIFEST"
-echo '@BINPATH@/apps/*'                           >> "$PACKAGE_MANIFEST"
+echo '@BINPATH@/sqlite-composer'                  >> "$PACKAGE_MANIFEST"
+echo '@RESPATH@/apps/*'                           >> "$PACKAGE_MANIFEST"
 echo '@RESPATH@/mozilla.cfg'                      >> "$PACKAGE_MANIFEST"
 echo '@RESPATH@/defaults/pref/sqlite-composer.js' >> "$PACKAGE_MANIFEST"
+sed -i '' '/@BINPATH@\/@MOZ_APP_NAME@-bin/d'         "$PACKAGE_MANIFEST"
 unset PACKAGE_MANIFEST
 
 # Patch the duplicate file error caused by including the sqlite-manager
@@ -107,6 +112,22 @@ ALLOWED_DUPES="$SOURCE_DIR/browser/installer/allowed-dupes.mn"
 echo 'apps/sqlite-manager/chrome/skin/default/images/close.gif' >> "$ALLOWED_DUPES"
 echo 'chrome/toolkit/skin/classic/global/icons/Close.gif'       >> "$ALLOWED_DUPES"
 unset ALLOWED_DUPES
+
+# Patch the Info.plist so that our launch script is executed instead of directly
+# executing the binary.
+# The awk command looks for the key ([<key>]CFBundleExecutable[</key>]), and
+# sets the value (the following line) with the name of the launcher script.
+
+FILE="$SOURCE_DIR/browser/app/macbuild/Contents/Info.plist.in"
+KEY='CFBundleExecutable'
+VALUE='<string>sqlite-composer</string>'
+
+awk "{print (f?\"$VALUE\":\$0); f=0} /$KEY/{f=1}" "$FILE" > "$FILE.tmp"
+mv "$FILE.tmp" "$FILE"
+
+unset FILE
+unset KEY
+unset VALUE
 
 
 # PATCH: DISABLED UPDATER BUG
@@ -122,20 +143,20 @@ unset ALLOWED_DUPES
 GUARD_OPEN='ifdef MOZ_UPDATER'
 GUARD_CLOSE='endif'
 
-sed -i '.original' "/mv -f/i\\
+sed -i '' "/mv -f/i\\
   $GUARD_OPEN\\
   " "$SOURCE_DIR/browser/app/Makefile.in"
-sed -i '.original' "/ln -s/a\\
+sed -i '' "/ln -s/a\\
   $GUARD_CLOSE\\
   " "$SOURCE_DIR/browser/app/Makefile.in"
 
 GUARD_OPEN='#ifdef MOZ_UPDATER'
 GUARD_CLOSE='#endif'
 
-sed -i '.original' "/LaunchServices/i\\
+sed -i '' "/LaunchServices/i\\
   $GUARD_OPEN\\
   " "$SOURCE_DIR/browser/installer/package-manifest.in"
-sed -i '.original' "/LaunchServices/a\\
+sed -i '' "/LaunchServices/a\\
   $GUARD_CLOSE\\
   " "$SOURCE_DIR/browser/installer/package-manifest.in"
 
@@ -146,18 +167,16 @@ unset GUARD_CLOSE
 # BUILD
 
 cp "$REPO_DIR/config/mozconfig" mozconfig
-read -p "About to configure..."
 ./mach configure
-read -p "About to build..."
 ./mach build 2>&1 | tee "$BUILD_DIR/build.log"
-read -p "Done building."
+read -p "Build done, waiting..."
 
 
 # CONFIGURE: PREFERENCES
 
-mkdir -p "$INSTALL_DIR/defaults/pref"
-cp "$REPO_DIR/config/sqlite-composer.js" "$INSTALL_DIR/defaults/pref"
-cp "$REPO_DIR/config/mozilla.cfg" "$INSTALL_DIR"
+mkdir -p "$BUNDLE_RES_DIR/defaults/pref"
+cp "$REPO_DIR/config/sqlite-composer.js" "$BUNDLE_RES_DIR/defaults/pref"
+cp "$REPO_DIR/config/mozilla.cfg" "$BUNDLE_RES_DIR"
 
 
 # INSTALL SQLITE MANAGER
@@ -179,7 +198,7 @@ cp -r "$EXT_DIR/source/"* "$APP_DIR"
 # Update the application.ini and other branding.
 
 cp "$EXT_DIR/application.ini" "$APP_DIR"
-grep -rl "SQLite Manager" "$APP_DIR" | xargs sed -i '.original' 's/SQLite Manager/SQLite Composer/g'
+grep --null -rl "SQLite Manager" "$APP_DIR" | xargs -0 sed -i '' 's/SQLite Manager/SQLite Composer/g'
 
 # Update the icons.
 
@@ -196,10 +215,14 @@ cp "$EXT_DIR/icons/icon_128x128.png" "$APP_DIR/icons/mozicon128.png"
 # in the repo; if we upgrade SQLite Manager, we should update this number as
 # well.
 
-sed -i '.original' 's/^.*SmAppInfo.extVersion =.*$/SmAppInfo.extVersion = (addon || {}).version || "0.8.3";/' "$APP_DIR/chrome/resource/appInfo.js"
+sed -i '' 's/^.*SmAppInfo.extVersion =.*$/SmAppInfo.extVersion = (addon || {}).version || "0.8.3";/' "$APP_DIR/chrome/resource/appInfo.js"
+
+
+# INSTALL LAUNCHER
+
+cp "$REPO_DIR/mac/launcher.sh" "$BUNDLE_BIN_DIR/sqlite-composer"
 
 
 # PACKAGE
 
-read -p "About to build installer..."
-./mach build installer
+./mach package
---
+++ mac/launcher.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BIN_DIR="$ROOT_DIR/../MacOS"
+RES_DIR="$ROOT_DIR/../Resources"
+
+# Noramlize the binary and resource directories.
+# If you're questioning why this is necessary, try commenting these out and
+# running the app; it'll crash and leave you with "Couldn't load XPCOM."
+# While I'm not 100%, I suspect this is security related, and that macOS is
+# blocking the dylibs from loading. After removing any ".." from the binary
+# path, everything seems to be cool.
+BIN_DIR="`cd \"$BIN_DIR\"; pwd`"
+RES_DIR="`cd \"$RES_DIR\"; pwd`"
+
+APP_DIR="$RES_DIR/apps/sqlite-manager"
+PROFILE_DIR="$RES_DIR/profiles/sqlite-composer"
+
+mkdir -p "$PROFILE_DIR"
+"$BIN_DIR/sqlite-composer-bin" --app "$APP_DIR/application.ini" --no-remote --profile "$PROFILE_DIR"
