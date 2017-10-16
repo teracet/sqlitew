e6c62478a29d44b879d83c176f8e2053bb72e418
Jamie Syme <jamie.d.syme@gmail.com>
2017-10-15 21:42:53 -0700

    Simplified bootstrap script.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,51 +4,23 @@ set -euo pipefail
 REPO_SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "$REPO_SCRIPTS_DIR/common.sh"
 
-cd "$FF_SOURCE_DIR"
 
-# In order to build Firefox, we need to install all of the libraries/tools it
-# depends on.
-# Fortunately, Firefox provides a convenient tool for us. Unfortunately, this
-# tool is not well-suited to automation, so we need to force feed it some
-# responses. These responses differ slightly from OS to OS.
-# TODO: Not sure about other OSes, but at least on Mac, running mach bootstrap
-# multiple times requires a different number of parameters.
-case "$BUILD_OS" in
-	linux)
-		# Responses:
-		#   2 - Build Firefox for Desktop
-		#   3 - Do not install Mercurial
-		#   1 - Create shared state directory for mozilla tools
-		printf "2\n3\n1\n" | ./mach bootstrap
-		;;
+# Install tools needed to run bootstrap script
 
-	mac)
-		# Part of the bootstrap process requires Xcode. If Xcode was installed but not
-		# used yet, we need to specify the app directory.
-		log 'Selecting Xcode app directory (requires sudo privileges)'
-		sudo 'xcode-select' --switch '/Applications/Xcode.app' || error_exit "xcode-select failed"
+if [[ "$BUILD_OS" = "mac" ]] ; then
+	# Part of the bootstrap process requires Xcode. If Xcode was installed
+	# but not used yet, we need to specify the app directory.
+	log 'Selecting Xcode app directory (requires sudo privileges)'
+	sudo 'xcode-select' --switch '/Applications/Xcode.app' || error_exit "xcode-select failed"
 
-		# Bootstrapping fails when installing rust on a Mac, so we will install it
-		# manually ahead of time.
-		log 'Installing rust'
-		brew install rust || error_exit "failed to install rust"
+	# Bootstrapping fails when installing rust on a Mac, so we will install
+	# it manually ahead of time.
+	log 'Installing rust'
+	brew install rust || error_exit "failed to install rust"
+fi
 
-		# Responses:
-		#   2 - Build Firefox for Desktop
-		#   1 - Create shared state directory for mozilla tools
-		#   2 - Do not configure Mercurial
-		printf "2\n1\n2\n" | ./mach bootstrap
-		;;
 
-	windows)
-		# Responses:
-		#   2 - Build Firefox for Desktop
-		#   1 - Create shared state directory for mozilla tools
-		#   2 - Do not configure Mercurial
-		printf "2\n1\n2\n" | ./mach bootstrap
-		;;
+# Run bootstrap script
 
-	*)
-		error_exit "Unrecognized or unsupported OS: $BUILD_OS"
-		;;
-esac
+cd "$FF_SOURCE_DIR/python/mozboot/bin"
+python bootstrap.py --application-choice=browser --no-interactive
