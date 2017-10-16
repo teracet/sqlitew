0176cfc1d28747f658067bd56769726d1d76d7a4
Jamie Syme <jamie.d.syme@gmail.com>
2017-10-16 01:20:28 -0700

    Fixed incorrect profile locations in launcher scripts.

--- config/linux-launcher.sh
+++ config/linux-launcher.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 APP_DIR="$ROOT_DIR/apps/sqlite-manager"
@@ -8,7 +8,6 @@ ICON_PATH="$APP_DIR/icons/mozicon128.png"
 sed -i -e "s|^Icon=$|Icon=$ICON_PATH|" "$ROOT_DIR/sqlite-composer.desktop"
 
 # Launch the app
-PROFILE_DIR="~/.teracet/sqlite-composer/profile"
+PROFILE_DIR="$HOME/.teracet/sqlite-composer/profile"
 mkdir -p "$PROFILE_DIR"
-PROFILE_DIR="$(cd "$PROFILE_DIR" && pwd)"
 "$ROOT_DIR/sqlite-composer-bin" --app "$APP_DIR/application.ini" --no-remote --profile "$PROFILE_DIR"
--- config/mac-launcher.sh
+++ config/mac-launcher.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BIN_DIR="$ROOT_DIR/../MacOS"
@@ -14,7 +14,6 @@ BIN_DIR="`cd \"$BIN_DIR\"; pwd`"
 RES_DIR="`cd \"$RES_DIR\"; pwd`"
 
 APP_DIR="$RES_DIR/apps/sqlite-manager"
-PROFILE_DIR="~/.teracet/sqlite-composer/profile"
+PROFILE_DIR="$HOME/.teracet/sqlite-composer/profile"
 mkdir -p "$PROFILE_DIR"
-PROFILE_DIR="$(cd "$PROFILE_DIR" && pwd)"
 "$BIN_DIR/sqlite-composer-bin" --app "$APP_DIR/application.ini" --no-remote --profile "$PROFILE_DIR"
